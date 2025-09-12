450278763618836eb9bf270ea58b198063aa6fa7
Thomas van der Linden <t.van.der.linden@amsterdam.nl>
2025-09-12 20:20:05 +0200

    gebruik van username werkend gemaakt

--- settings_template.py
+++ settings_template.py
@@ -1,9 +1,11 @@
 # dit bestand bevat instellingen voor de tool
-# pas de waarden aan voor je eigen systeem
+# pas de waarden als dat nodig is aan voor je eigen systeem
 # sla het bestand op als settings.py en voeg settings.py niet toe aan de repository
+import getpass
+username = getpass.getuser()
 
-basis_folder_in = "C:/Users/%USERNAME%/OneDrive - Gemeente Amsterdam"
-basis_folder_uit = "C:/Users/%USERNAME%/OneDrive - Gemeente Amsterdam"
+basis_folder_in = f"C:/Users/{username}/OneDrive - Gemeente Amsterdam"
+basis_folder_uit = f"C:/Users/{username}/OneDrive - Gemeente Amsterdam"
 check_al_gedaan = True
 decimal = "."
 sep = ";"
