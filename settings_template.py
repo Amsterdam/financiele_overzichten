ebbe79127d8f9705211ceb15c88f425fcee4adcc
Maarten Soudijn <m.soudijn@amsterdam.nl>
2025-09-29 15:34:20 +0200

    eentje gemist - nu dan

--- settings_template.py
+++ settings_template.py
@@ -9,7 +9,7 @@ folder_in = f"C:/Users/{username}/OneDrive - Gemeente Amsterdam/Team Data, Rappo
 folder_uit = f"C:/Users/{username}/OneDrive - Gemeente Amsterdam/Data/AFIS/FIP/CSV_data"
 # voor T. en M. (en anderen met toegang tot "FIP/CSV_data" van J.T.) ...
 # ... comment regel 9 en uncomment regel 12
-# basis_folder_uit = f"C:/Users/{username}/OneDrive - Gemeente Amsterdam/Bestanden van Tse, Jeffrey - FIP/CSV_data"
+# folder_uit = f"C:/Users/{username}/OneDrive - Gemeente Amsterdam/Bestanden van Tse, Jeffrey - FIP/CSV_data"
 check_al_gedaan = True
 decimal = "."
 sep = ";"
