9e80093d07ca09c0d9cd670142f44f4bcd5d3c95
Maarten Soudijn <m.soudijn@amsterdam.nl>
2025-10-02 16:28:23 +0200

    Settings_template.py aangepast en een testmap toegevoegd voor het inlezen van minder bestanden. Bij het testen kwam ik echter een fout tegen in het maken van de kolommen_tabel. Bij meerdere tests bleek er iets mis te gaan met de index en het weggooien van de duplicates. Kleine aanpassingen gedaan om dit op te lossen.

--- settings_template.py
+++ settings_template.py
@@ -1,15 +1,21 @@
 # dit bestand bevat instellingen voor de tool
+# onderstaande waarden zijn nu ingesteld voor gebruik door J.T.
 # pas de waarden als dat nodig is aan voor je eigen systeem
 # sla het bestand op als settings.py en voeg settings.py niet toe aan de repository
 import getpass
 username = getpass.getuser()
 
-folder_in = f"C:/Users/{username}/OneDrive - Gemeente Amsterdam/Team Data, Rapportages en Innovatie (DR&I) - Boekingsregels"
-# voor J.T.
-folder_uit = f"C:/Users/{username}/OneDrive - Gemeente Amsterdam/Data/AFIS/FIP/CSV_data"
-# voor T. en M. (en anderen met toegang tot "FIP/CSV_data" van J.T.) ...
-# ... comment regel 9 en uncomment regel 12
-# folder_uit = f"C:/Users/{username}/OneDrive - Gemeente Amsterdam/Bestanden van Tse, Jeffrey - FIP/CSV_data"
+# basismap
+base_folder = f"C:/Users/{username}/OneDrive - Gemeente Amsterdam"
+# testmap voor inlezen
+folder_in_test = f"{base_folder}/Data/AFIS/FIP"
+# map voor inlezen van alle excel bestanden (indien toegevoegd aan persoonlijk OneDrive map, zie README.md)
+folder_in = f"{base_folder}/Team Data, Rapportages en Innovatie (DR&I) - Boekingsregels"
+# map voor schrijven
+folder_uit = f"{base_folder}/Data/AFIS/FIP/CSV_data"
+# voor testdraaien zet test op True, anders op False
+test = True
+# voor het doorlopen van alle bestanden bij het inlezen, zet check_al_gedaan op False
 check_al_gedaan = True
 decimal = "."
 sep = ";"
